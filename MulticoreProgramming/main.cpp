f3b10723b02d5568247d14868515156415513f01
sunkue <wmwwmmm@naver.com>
2022-09-27 19:05:47 +0900

    솔루션 탐색기 보기좋게 정리

--- MulticoreProgramming/main.cpp
+++ MulticoreProgramming/main.cpp
@@ -16,17 +16,7 @@
 #include <chrono>
 
 #include "CAS.hpp"
-
-namespace timer {
-	using clk = std::chrono::high_resolution_clock;
-	using namespace std::chrono;
-	using namespace std::literals::string_view_literals;
-	clk::time_point static thread_local StopWatch;
-	inline void reset() { StopWatch = clk::now(); }
-	inline void elapsed(const std::string_view header = ""sv, const std::string_view footer = "\n"sv) {
-		std::cout << header << duration_cast<milliseconds>(clk::now() - StopWatch) << footer;
-	}
-}
+#include "Timer.hpp"
 
 using namespace std;
 
